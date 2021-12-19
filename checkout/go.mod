4e260ea04980b5c8a52a349d234d9e6c4ddaf3c5
Pablo Morelli <pabmorelli@gmail.com>
2021-12-19 14:33:37 +0100

    added checkout

---
+++ checkout/go.mod
@@ -0,0 +1,26 @@
+module github.com/pmorelli92/open-telemetry-go/checkout
+
+go 1.17
+
+require (
+	github.com/pmorelli92/open-telemetry-go/proto v0.0.0-20211219114238-87d73983f1cf
+	github.com/pmorelli92/open-telemetry-go/utils v0.0.0-20211219114238-87d73983f1cf
+	github.com/streadway/amqp v1.0.0
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.28.0
+	go.opentelemetry.io/otel v1.3.0
+	go.opentelemetry.io/otel/trace v1.3.0
+	google.golang.org/grpc v1.42.0
+)
+
+require (
+	github.com/go-logr/logr v1.2.1 // indirect
+	github.com/go-logr/stdr v1.2.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	go.opentelemetry.io/otel/exporters/jaeger v1.2.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
+	golang.org/x/text v0.3.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ checkout/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "github.com/pmorelli92/open-telemetry-go/proto"
+	"github.com/pmorelli92/open-telemetry-go/utils"
+	"github.com/streadway/amqp"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	jaegerAddress := utils.EnvString("JAEGER_ADDRESS", "localhost")
+	jaegerPort := utils.EnvString("JAEGER_PORT", "6831")
+	grpcAddress := utils.EnvString("GRPC_ADDRESS", "localhost:8080")
+	amqpUser := utils.EnvString("RABBITMQ_USER", "guest")
+	amqpPass := utils.EnvString("RABBITMQ_PASS", "guest")
+	amqpHost := utils.EnvString("RABBITMQ_HOST", "localhost")
+	amqpPort := utils.EnvString("RABBITMQ_PORT", "5672")
+
+	err := utils.SetGlobalTracer("checkout", jaegerAddress, jaegerPort)
+	if err != nil {
+		log.Fatalf("failed to create tracer: %v", err)
+	}
+
+	channel, closeConn := utils.ConnectAmqp(amqpUser, amqpPass, amqpHost, amqpPort)
+	defer closeConn()
+
+	lis, err := net.Listen("tcp", grpcAddress)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(otelgrpc.UnaryServerInterceptor()),
+		grpc.StreamInterceptor(otelgrpc.StreamServerInterceptor()))
+
+	pb.RegisterCheckoutServer(s, &server{channel: channel})
+
+	log.Printf("GRPC server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+type server struct {
+	pb.UnimplementedCheckoutServer
+	channel *amqp.Channel
+}
+
+func (s *server) DoCheckout(ctx context.Context, rq *pb.CheckoutRequest) (*pb.CheckoutResponse, error) {
+	messageName := "checkout.processed"
+
+	// Create a new span (child of the trace id) to inform the publishing of the message
+	tr := otel.Tracer("amqp")
+	amqpContext, messageSpan := tr.Start(ctx, fmt.Sprintf("AMQP - publish - %s", messageName))
+	defer messageSpan.End()
+
+	// Inject the context in the headers
+	headers := utils.InjectAMQPHeaders(amqpContext)
+	msg := amqp.Publishing{Headers: headers}
+	err := s.channel.Publish("exchange", messageName, false, false, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response := &pb.CheckoutResponse{TotalAmount: 1234}
+
+	// Example on how to log specific events for a span
+	span := trace.SpanFromContext(ctx)
+	span.AddEvent(fmt.Sprintf("response: %v", response))
+
+	return response, nil
+}
